45aed79a389043164dc4cb452df2dfb7ba7aea66
Shilpi Agarwal <shilpi.agarwal@imfcorp.com>
2018-04-26 15:15:17 +0530

    file changes

--- NtuAlumniPortal1/src/page/classes/LoginPage.java
+++ NtuAlumniPortal1/src/page/classes/LoginPage.java
@@ -4,36 +4,93 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage{
+	
+	WebDriver driver;
 	
 	
 	public static WebElement element= null;
-	private WebDriver driver;
 	
-
-	public static WebElement linkedinButton(WebDriver driver){
+	public static WebElement linkedinLogin(WebDriver driver){
 		element= driver.findElement(By.id("dnn_ctr532_ntuLogin_Login_Linkedin_loginButton"));
 		return element;
 		
-    }
-	
-	public static void clicklindekinButton(WebDriver driver){
 		
-		element = linkedinButton(driver);
-		element.click();
 	}
 	
-	public static WebElement facebookButton(WebDriver driver){
+	public static WebElement facebookLogin(WebDriver driver){
 	
 		element= driver.findElement(By.id("dnn_ctr532_ntuLogin_Login_Facebook_loginButton"));
 		return element;
-	}
+		
 	
-	public static  void clickfacebookButton(WebDriver driver){
-		element = facebookButton(driver);
-		element.click();
 	}
-	
 
+	
+	@FindBy(name= "session_key")
+    WebElement linkedinemailTextbox ;
+	
+	@FindBy(id="session_password-oauth2SAuthorizeForm")
+	WebElement linkedinpasswordTextbox;
+	
+	@FindBy(name= "authorize")
+	WebElement linkedinAllowAccessButton;
+	
+	@FindBy(linkText= "http://ntualumniportal.excelindia.com/NTULogin/Admin?error=access_denied&amp;error_description=the+user+denied+your+request&amp;state=Linkedin#!")
+	WebElement linkedinCancelButton;
+	
+	@FindBy(id= "email")
+	WebElement facebookEmailTextbox;
+	
+	@FindBy(id="pass")
+	WebElement facebookPasswordTextbox;
+	
+	@FindBy(id= "loginbutton")
+	WebElement facebookLoginButton;
+	
+	
+	
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void linkedinEnterEmail(String email) {
+		
+		linkedinemailTextbox.sendKeys(email);
+	}
+	
+	public void linkedinEnterPassword(String password) {
+		
+		linkedinpasswordTextbox.sendKeys(password);
+	}
+	
+	public void linkedinClickAllowAccessButton(){
+		
+		linkedinAllowAccessButton.click();
+	}
+	
+	public void linkedinClickCancelButton(){
+		linkedinCancelButton.click();
+	}
+	
+	public void facebookEnterEmail(String email){
+		
+		facebookEmailTextbox.sendKeys(email);
+	}
+	
+	public void facebookEnterPassword(String password){
+		
+		facebookPasswordTextbox.sendKeys(password);
+	}
+	
+	public void facebookClickLoginButton(){
+		
+		facebookLoginButton.click();
+	}
+	
+	
 }
--- NtuAlumniPortal1/src/page/classes/TestNG.java
+++ NtuAlumniPortal1/src/page/classes/TestNG.java
@@ -1,55 +1,79 @@
 package page.classes;
 
 import org.testng.annotations.Test;
-
 import page.classes.LoginPage;
-
-
+//import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 import java.util.concurrent.TimeUnit;
+
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 
 public class TestNG {
 	
-	private static  WebDriver driver;
+	private WebDriver driver;
 	private String baseURL = "http://ntualumniportal.excelindia.com/NTULogin";
-	private static String social= "linkedin";
-
+	private String social= "facebook";
+	LoginPage loginPage;
 
  
 	
   @BeforeSuite
   public void beforeClass() {
 
-	 System.setProperty("webdriver.chrome.driver","C:\\Users\\shilpi.agarwal\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
+	 System.setProperty("webdriver.gecko.driver","D:\\Eclipse\\geckodriver-v0.16.1-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		 driver.get(baseURL);
-	   }
+	   driver.get(baseURL);
+	   
+	   loginPage = new LoginPage(driver);
+	  
+  }
   
- 
+  public void clickLinkedin() throws InterruptedException {
+	  
+	  LoginPage.linkedinLogin(driver).click();
+	  loginPage.linkedinEnterEmail("ntualumniuser1@gmail.com");
+	  loginPage.linkedinEnterPassword("Excel@123");
+	  loginPage.linkedinClickAllowAccessButton();
+	  Thread.sleep(100000);
+	 }
+  
+  public void clickFacebook() throws InterruptedException{
+	  
+	  LoginPage.facebookLogin(driver).click();
+	  loginPage.facebookEnterEmail("ntualumniuser1@gmail.com");
+	  loginPage.facebookEnterPassword("Excel@123");
+	  loginPage.facebookClickLoginButton();
+	  Thread.sleep(10000);
+  }
+  
   @Test
-  public static void socialLogin() throws InterruptedException {
+  public void clickSocial() throws InterruptedException {
 	  
 	  if(social.equals("linkedin")) {
 		  
-		  LoginPage.clicklindekinButton(driver);
+		  this.clickLinkedin();
 		  Thread.sleep(2000);
 	  }
 	  
 	  else {
 		  
-		  LoginPage.clickfacebookButton(driver);
+		  this.clickFacebook();
 	  }
 	    
   }
 
   @AfterSuite
   public void afterClass() {
-	  driver.quit();
+	 // driver.quit();
   }
 
 }
